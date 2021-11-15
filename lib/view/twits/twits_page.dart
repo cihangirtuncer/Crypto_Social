51c9c13137aebb352e3a948c7bc65cc6e865dc3d
Cihangir Tuncer <cihangiraa@cntbilisim.com.tr>
2021-11-15 23:48:09 +0300

    push

--- lib/view/twits/twits_page.dart
+++ lib/view/twits/twits_page.dart
@@ -80,10 +80,12 @@ class TwitsScreens extends StatelessWidget {
                   Icon(Icons.add),
                   CircleAvatar(
                     backgroundColor: Colors.blue,
-                    child: Text("TWIT",
-                        style: TextStyle(
-                          color: Colors.black,
-                        )),
+                    child: Text(
+                      "TWIT",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
                   )
                 ],
               )
